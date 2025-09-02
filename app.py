e7993ee7a0df03a093c86c38ce6ca99e2f09aa5d
kitsadapan863 <Kitsadapankangna685@gmail.com>
2025-09-02 15:28:33 +0700

    install gunicorn

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Jenkins + Python CI/CD!"
+    return "Hello Render + Jenkins!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
+
